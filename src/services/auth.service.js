543d7c47d9d5fefd0ae8776b4e93913a428946ff
sarathta <sarath@aicnet.in>
2026-06-25 14:14:25 +0530

    register removed

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -2,24 +2,6 @@ const {prisma} = require("../config/db");
 const bcrypt = require("bcryptjs");
 const {generateToken} = require("../utils/generateToken");
 
-async function register(name,email,password) {
-    const userExists = await prisma.users.findUnique({
-        where: {email: email}
-    });
-    if (userExists) {
-        throw new Error("User already exists");
-    }
-    // Hash password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password,salt);
-    // Create user
-    const newUser = await prisma.users.create({
-        data: {name,email,password:hashedPassword}
-    });
-    // Return user
-    return {id: newUser.id, name: newUser.name, email: newUser.email};
-}
-
 async function login(username,password) {
     // Check if user exists
     const user = await prisma.mst_employees.findUnique({
@@ -57,7 +39,6 @@ async function logout(res) {
 }
 
 module.exports = {
-    register,
     login,
     logout
 }
